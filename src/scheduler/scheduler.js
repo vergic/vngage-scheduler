a91a03e26938fc0bf876deb66e9ed2e0bea6655c
Tobias <tobias@vergic.com>
2021-04-15 13:03:23 +0200

    Fixed fallback to native timers in Firefox if the worker is blocked by CSP

--- src/scheduler/scheduler.js
+++ src/scheduler/scheduler.js
@@ -1,7 +1,15 @@
 import { timerWorkerSrc } from '../worker/timerWorkerSrc';
 
 const timers = {};
-let scheduler;
+const nativeScheduler = {
+    setInterval: (fn, interval) => window.setInterval(fn, interval),
+    setTimeout: (fn, timeout) => window.setTimeout(fn, timeout),
+    clearInterval: (id) => window.clearInterval(id),
+    clearTimeout: (id) => window.clearTimeout(id),
+    _debug: () => { console.log('Scheduler fallback to native timers'); }
+}
+let useTimerWorker = false;
+let scheduler = nativeScheduler; // Use native timers by default... Switch to worker timers when the worker is verified to actually work!
 
 try {
     const blob = new Blob([timerWorkerSrc], {type: 'application/javascript; charset=utf-8'});
@@ -32,6 +40,9 @@ try {
 
         return {
             setInterval: (fn, interval) => {
+                if (!useTimerWorker) {
+                    return nativeScheduler.setInterval(fn, interval);
+                }
                 var id = getRndId();
                 timers[id] = fn;
                 timerWorker.postMessage({
@@ -42,13 +53,21 @@ try {
                 return id;
             },
             clearInterval: (id) => {
+                if (!useTimerWorker) {
+                    return nativeScheduler.clearInterval(id);
+                }
                 timerWorker.postMessage({
                     action: 'clearInterval',
                     id: id
                 });
                 delete timers[id];
             },
-            setTimeout: (fn, timeout) => {
+            setTimeout: (fn, timeout, force) => {
+                if (!useTimerWorker && !force) {
+                    // The "force"-flag is used to override "useTimerWorker" to be able to test/verify that the worker is actually working...
+                    // Only used after calling load() below...
+                    return nativeScheduler.setTimeout(fn, timeout);
+                }
                 var id = getRndId();
                 timers[id] = () => {
                     delete timers[id];
@@ -62,6 +81,9 @@ try {
                 return id;
             },
             clearTimeout: (id) => {
+                if (!useTimerWorker) {
+                    return nativeScheduler.clearTimeout(id);
+                }
                 timerWorker.postMessage({
                     action: 'clearTimeout',
                     id: id
@@ -69,6 +91,9 @@ try {
                 delete timers[id];
             },
             _debug: () => {
+                if (!useTimerWorker) {
+                    return nativeScheduler._debug();
+                }
                 timerWorker.postMessage({
                     action: 'debug'
                 });
@@ -78,19 +103,18 @@ try {
     }
 
     scheduler = load(url);
-    scheduler.setTimeout(() => URL.revokeObjectURL(url), 0);
+    scheduler.setTimeout(() => {
+        // Revoke the blob and TEST that the timer works!
+        // (Firefox will silently fail if the worker is blocked by CSP, so don't start using it until it's verified to actually work!)
+        useTimerWorker = true;
+        URL.revokeObjectURL(url);
+    }, 0, true);
     // console.log('Scheduler init successful');
 } catch (e) {
     // If (for some reason) init fails: Map scheduler functions to native timer functions.
     // NOTE: extra parameters to setTimeout() and setInterval() is not supported
     // console.error('Scheduler init failed. Fallback to native timers');
-    scheduler = {
-        setInterval: (fn, interval) => window.setInterval(fn, interval),
-        setTimeout: (fn, timeout) => window.setTimeout(fn, timeout),
-        clearInterval: (id) => window.clearInterval(id),
-        clearTimeout: (id) => window.clearTimeout(id),
-        _debug: () => {console.log('Scheduler fallback to native timers')}
-    };
+    scheduler = nativeScheduler;
 }
 
 export default scheduler;
